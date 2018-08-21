4c6af6cd6ad77a759038a114a656664936d24665
PepseeActus <christophe.sbastien@gmail.com>
2018-08-21 10:32:24 +0200

    add html

---
+++ currency.js
@@ -0,0 +1,13 @@
+var converter = CurrencyConverter({
+    API : {
+        url: 'http://apilayer.net/api/list',
+        queryParams: {
+            apiKey: '476a103e3541250b9aa2b704e0ac4185'
+        }
+    }
+});
+
+function loadCurrencies(){
+  var from = getElementById('from');
+  var to = getElementById('to');
+}
